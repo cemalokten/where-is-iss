0aab7974368495de843e1c22b1a394e7254fa94b
cemalokten <cemalokten@gmail.com>
2021-08-06 08:30:22 +0100

    API fetchs data and displays it

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 'use strict';
 
+const body = document.getElementsByTagName('body');
+const marqueeText = Array.from(document.getElementsByClassName('content'));
+marqueeText.forEach((text) => (text.innerHTML = text.innerHTML.repeat(10)));
+const locationText = Array.from(document.getElementsByClassName('location--js'));
+const velocityText = Array.from(document.getElementsByClassName('velocity--js'));
+
 let longitude = null;
 let latitude = null;
 
@@ -8,18 +14,39 @@ function assignLongitude(long, lat) {
   latitude = lat;
 }
 
-// function changeColour(value) {
-//   if (value) {
-//   }
-// }
+function changeColourAndLocation(array, value) {
+  array.forEach((c) => {
+    if (value) {
+      c.textContent = 'SEA';
+    } else {
+      c.textContent = 'LAND';
+    }
+  });
+
+  if (value) {
+    body[0].style.backgroundColor = 'var(--sea)';
+  } else {
+    body[0].style.backgroundColor = 'var(--land)';
+  }
+}
+
+function changeVelocity(array, num) {
+  array.forEach((e) => {
+    e.textContent = `${Math.round(num * 100) / 100} km/h`;
+  });
+}
 
 fetch('https://api.wheretheiss.at/v1/satellites/25544')
   .then((response) => response.json())
   .then((data) => {
     assignLongitude(data.longitude, data.latitude);
-    return fetch(
-      `https://api.onwater.io/api/v1/results/${latitude},${longitude}?access_token=-yxsyX6EsS4ysebkXji6`
-    );
+
+    changeVelocity(velocityText, data.velocity);
+
+    return fetch(`https://api.onwater.io/api/v1/results/${latitude},${longitude}?access_token=-yxsyX6EsS4ysebkXji6`);
   })
   .then((response) => response.json())
-  .then((something) => console.log(something.water));
+  .then((landOrSea) => changeColourAndLocation(locationText, landOrSea));
+
+//  - THE ISS IS OVER <span class='location--js'></span> TRAVELLING AT <span
+//                       class='velocity--js'></span>
