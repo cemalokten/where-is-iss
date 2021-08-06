4825fa77f55fa935b98aa0f07899fe594e384729
cemalokten <cemalokten@gmail.com>
2021-08-06 11:06:30 +0100

    Working version

--- script.js
+++ script.js
@@ -2,51 +2,60 @@
 
 const body = document.getElementsByTagName('body');
 const marqueeText = Array.from(document.getElementsByClassName('content'));
+let detailColour = null;
 marqueeText.forEach((text) => (text.innerHTML = text.innerHTML.repeat(10)));
-const locationText = Array.from(document.getElementsByClassName('location--js'));
-const velocityText = Array.from(document.getElementsByClassName('velocity--js'));
 
 let longitude = null;
 let latitude = null;
+let landOrSeaBol = null;
+let velocity = null;
 
-function assignLongitude(long, lat) {
+function assignLongitude(long, lat, vel) {
   longitude = long;
   latitude = lat;
+  velocity = Math.round(vel * 100) / 100;
 }
 
-function changeColourAndLocation(array, value) {
-  array.forEach((c) => {
-    if (value) {
-      c.textContent = 'SEA';
-    } else {
-      c.textContent = 'LAND';
-    }
-  });
-
+function changeColourAndLocation(value) {
   if (value) {
+    landOrSeaBol = 'SEA';
     body[0].style.backgroundColor = 'var(--sea)';
   } else {
+    landOrSeaBol = 'LAND';
     body[0].style.backgroundColor = 'var(--land)';
   }
 }
 
-function changeVelocity(array, num) {
-  array.forEach((e) => {
-    e.textContent = `${Math.round(num * 100) / 100} km/h`;
+function setTextColour(val) {
+  detailColour.forEach((c) => {
+    if (val == 'SEA') {
+      c.style.color = 'var(--sea)';
+    } else {
+      c.style.color = 'var(--land)';
+    }
   });
+}
+
+function buildMarquee(vel, landOrSea) {
+  return `THE ISS IS OVER <span class='detail'>${landOrSeaBol}</span> TRAVELLING AT <span class='detail'>${velocity} km/h</span> → `;
 }
 
 fetch('https://api.wheretheiss.at/v1/satellites/25544')
   .then((response) => response.json())
   .then((data) => {
-    assignLongitude(data.longitude, data.latitude);
-
-    changeVelocity(velocityText, data.velocity);
+    assignLongitude(data.longitude, data.latitude, data.velocity);
 
     return fetch(`https://api.onwater.io/api/v1/results/${latitude},${longitude}?access_token=-yxsyX6EsS4ysebkXji6`);
   })
   .then((response) => response.json())
-  .then((landOrSea) => changeColourAndLocation(locationText, landOrSea));
+  .then((landOrSea) => changeColourAndLocation(landOrSea.water))
+  .then(() => {
+    marqueeText.forEach((text) => (text.innerHTML = buildMarquee(velocity, landOrSeaBol).repeat(45)));
+  })
+  .then(() => {
+    detailColour = Array.from(document.getElementsByClassName('detail'));
+    setTextColour(landOrSeaBol);
+  });
 
 //  - THE ISS IS OVER <span class='location--js'></span> TRAVELLING AT <span
 //                       class='velocity--js'></span>
